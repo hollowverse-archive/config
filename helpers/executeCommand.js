3317af51eb050aa2ce59bd6cbc5b99e93ea55b1f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-27 18:51:50 +0300

    Extract single command execution logic to a separate module

---
+++ helpers/executeCommand.js
@@ -0,0 +1,29 @@
+const shelljs = require('shelljs');
+
+/**
+ * A helper function that execute a single shell command or JavaScript function.
+ * Supports asynchronous JS functions, and executes shell commands in a child process
+ * that the event loop is not blocked while executing the command.
+ * @param  {string | function(): (number | Promise<number>)} command
+ * @return Exit code for the executed command, a non-zero value indicates failure
+ */
+module.exports = async function executeCommands(command) {
+  let code = 0;
+  if (typeof command === 'function') {
+    if (command.name) {
+      console.info(`${command.name}()`);
+    }
+    try {
+      code = await Promise.resolve(command());
+    } catch (e) {
+      console.error(e);
+      code = 1;
+    }
+  } else {
+    const shellCommand = command.replace('\n', '').replace(/\s+/g, ' ').trim();
+    console.info(shellCommand);
+    code = await new Promise(resolve => shelljs.exec(shellCommand, resolve));
+  }
+
+  return code;
+};
--- helpers/executeCommands.js
+++ helpers/executeCommands.js
@@ -1,4 +1,4 @@
-const shelljs = require('shelljs');
+const executeCommand = require('./executeCommand');
 
 /**
  * A helper function that executes shell commands or JavaScript functions.
@@ -10,27 +10,8 @@ const shelljs = require('shelljs');
 module.exports = async function executeCommands(commands) {
   // eslint-disable-next-line no-restricted-syntax
   for (const command of commands) {
-    let code = 0;
-    if (typeof command === 'function') {
-      if (command.name) {
-        console.info(`${command.name}()`);
-      }
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        code = await Promise.resolve(command());
-      } catch (e) {
-        console.error(e);
-        code = 1;
-      }
-    } else {
-      const shellCommand = command
-        .replace('\n', '')
-        .replace(/\s+/g, ' ')
-        .trim();
-      console.info(shellCommand);
-      // eslint-disable-next-line no-await-in-loop
-      code = await new Promise(resolve => shelljs.exec(shellCommand, resolve));
-    }
+    // eslint-disable-next-line no-await-in-loop
+    const code = await executeCommand(command);
 
     // Return on first non-zero exit value
     if (code !== 0) {
