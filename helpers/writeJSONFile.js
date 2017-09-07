98aaeaeef3dbe11812bbfc18645797dc26abdb95
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-07 18:44:21 +0300

    Replace writeEnvFile with generic writeJSONFile

---
+++ helpers/writeJSONFile.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const { promisify } = require('util');
+
+const writeFile = promisify(fs.writeFile);
+
+/**
+ * @param {string} path Full path to the file to be written, including the file name and extension
+ * @param {any} data JavaScript object to write, must be serializable
+ */
+module.exports = function writeJSONFile(path, data) {
+  return writeFile(path, data);
+};
