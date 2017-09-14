ba731d0000e4cbf688ab34e1e99de71d5f6f5d4e
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-14 21:44:38 +0300

    Add option to ignroe files

--- helpers/createZipFile.js
+++ helpers/createZipFile.js
@@ -10,11 +10,16 @@ const fs = require('fs');
  * the root of the file matching the current working directory.
  * @param {string} path The full path where the ZIP file will be saved, including the
  * @param {string[]} patterns Glob patterns of files to include in the ZIP file.
+ * @param {string[] | undefined} ignore Patterns of files to ignore
  * filename and extension
  */
-module.exports = async function createZipFile(path, patterns) {
+module.exports = async function createZipFile(
+  path,
+  patterns,
+  ignore = undefined,
+) {
   const files = flatten(
-    patterns.map(pattern => glob.sync(pattern, { nodir: true })),
+    patterns.map(pattern => glob.sync(pattern, { nodir: true, ignore })),
   );
   const zipFile = new JSZip();
 
