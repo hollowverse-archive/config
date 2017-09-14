6c2ba7c506509777de1da7cd702317e6cb6adcde
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-14 21:09:53 +0300

    Rename writeJSONFile to writeJsonFile

--- helpers/writeJsonFile.js
+++ helpers/writeJsonFile.js
@@ -7,6 +7,6 @@ const writeFile = promisify(fs.writeFile);
  * @param {string} path Full path to the file to be written, including the file name and extension
  * @param {any} data JavaScript object to write, must be serializable
  */
-module.exports = function writeJSONFile(path, data) {
+module.exports = function writeJsonFile(path, data) {
   return writeFile(path, JSON.stringify(data, undefined, 2));
 };
